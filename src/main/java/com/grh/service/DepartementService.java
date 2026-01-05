3983154b7692e2e5e0209fcad0f8c03205441064
Your Name <you@example.com>
2026-01-05 20:54:53 +0100

    dernière modification

---
+++ src/main/java/com/grh/service/DepartementService.java
@@ -0,0 +1,30 @@
+package com.grh.service;
+
+import com.grh.dao.DepartementDao;
+import com.grh.dao.DepartementDaoImp;
+import com.grh.model.Departement;
+
+public class DepartementService {
+    private DepartementDao departementDao ;
+
+    public DepartementService(){
+        departementDao = new DepartementDaoImp();
+    }
+
+    public Boolean addDepartement(Departement departement){
+        return departementDao.addDepartment(departement.getNom(), departement.getDescription()) >  0 ;
+    }
+
+    public Departement findDepartementByName(String nom){
+        return departementDao.findDepartmentByName(nom );
+    }
+
+    public Boolean updateDepartement(Departement departement){
+        return departementDao.updateDepartement(departement) > 0 ;
+    }
+
+    public Boolean deleteDepartementById(int id){
+        return departementDao.deleteDepartement(id )> 0 ;
+    }
+
+}
